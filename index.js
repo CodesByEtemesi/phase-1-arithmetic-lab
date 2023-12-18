cd237311ea1fc2bc8167e33444ce32dad4be0391
ETEMESI <samweletemesi2@Gmail.com>
2023-12-18 06:09:35 +0300

    Arithmetic lab.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const multiply = 'num1 * num2';
+function arithmetic (multiply){
+    num1 === 31;
+    num2 === 2;
+}
+
+const random = 'num3' ;
+function arithmetic (add){
+    num3 > 0;  
+}
+
+console.log(multiply)
+
+
+
+
